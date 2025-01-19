1af66a429364eaa2c94edc03269a4717e08d2b3d
Okuda-Takuya-0223 <tamiwomax@gmail.com>
2025-01-19 14:01:39 +0900

    一覧機能作成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -7,10 +7,16 @@ use Illuminate\Http\Request;
 
 class MemoController extends Controller
 {
-    public function show($id) 
-    {
-    $memo = Memo::find($id);
-    return view("memos.show",["memo" =>$memo]);
+    public function index()
+    { //全件取得
+        $memos = Memo::all();
+        return view("memos.index", ["memos" => $memos]);
+    }
 
+
+    public function show($id)
+    {
+        $memo = Memo::find($id);
+        return view("memos.show", ["memo" => $memo]);
     }
 }
