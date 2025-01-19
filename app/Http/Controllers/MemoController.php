7a0a6b7a0e1d0a87135c7404f8e73c274201277e
Okuda-Takuya-0223 <tamiwomax@gmail.com>
2025-01-19 15:42:06 +0900

    投稿機能

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -7,6 +7,26 @@ use Illuminate\Http\Request;
 
 class MemoController extends Controller
 {
+    public function create()
+    {
+        return view("memos.create");
+    }
+
+    public function store(Request $request)
+    {
+    // インスタンスの作成
+    $memo = new Memo();
+
+        $memo->title = $request->title;
+        $memo->body = $request->body;
+
+        $memo->save();
+        //登録したらindexに戻る
+        return redirect("memos.index");
+    }
+
+
+
     public function index()
     { //全件取得
         $memos = Memo::all();
